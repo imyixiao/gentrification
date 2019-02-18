e93febb3725177c345121eedbae259f19257f5ff
Yi Xiao <xiaoyi.sybil@gmail.com>
2019-02-18 01:02:58 -0500

    data

--- inside_boundary_checker.py
+++ inside_boundary_checker.py
@@ -16,6 +16,7 @@ import fiona
 import shapely
 from shapely import geometry
 
+
 def inside_boundary_checker(shp_file_path, lat, lng):
     with fiona.open(shp_file_path) as fiona_collection:
         # In this case, we'll assume the shapefile only has one record/layer (e.g., the shapefile
@@ -23,6 +24,9 @@ def inside_boundary_checker(shp_file_path, lat, lng):
         shapefile_record = next(iter(fiona_collection))
         # Use Shapely to create the polygon
         shape = shapely.geometry.asShape( shapefile_record['geometry'] )
+        print(type(shape))
+        if type(shape) is shapely.geometry.multipolygon.MultiPolygonAdapter:
+            raise ValueError('multiple polygons have been created!')
         #create box to fast filter out data
         minx, miny, maxx, maxy = shape.bounds
         print("minx:" + str(minx) + ", miny:"+ str(miny) + ", maxx:" + str(maxx) + ", maxy" + str(maxy))
@@ -42,11 +46,26 @@ def inside_boundary_checker(shp_file_path, lat, lng):
                 return False
 
 
+def us_lower48_checker(lat, lng):
+    # http://en.wikipedia.org/wiki/Extreme_points_of_the_United_States#Westernmost
+    #https://gist.github.com/jsundram/1251783
+    top = 49.3457868 # north lat
+    left = -124.7844079 # west long
+    right = -66.9513812 # east long
+    bottom =  24.7433195 # south lat
+    if bottom <= lat <= top and left <= lng <= right:
+        return True
+    return False
+
+
 if __name__ == '__main__':
     shp_file_path = './data/cb_2017_us_nation_5m/cb_2017_us_nation_5m.shp'
+
     #restaurant 1, in canada 
-    print("restaurant in canada :" + "true" if inside_boundary_checker(shp_file_path,43.651070, -79.347015) else "false")
+    print("restaurant in canada :" + ("true" if us_lower48_checker(43.651070, -79.347015) else  "false"))
     #restaurant 2, in us
-    print("restaurant in u.s :" + "true" if inside_boundary_checker(shp_file_path,42.3012621, -83.7171617) else "false")
+    print("restaurant in u.s :" + ("true" if us_lower48_checker(42.3012621, -83.7171617) else  "false"))
     #restaurant 3, in mexican 
-    print("restaurant in mexican :" + "true" if inside_boundary_checker(shp_file_path,19.42847, -99.12766) else "false")
+    print("restaurant in mexican :" + ("true" if us_lower48_checker(19.42847, -99.12766) else  "false"))
+    
+    
