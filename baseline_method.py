f68acbc1cd3ab4397be002225544e9df37840cda
Yi Xiao <xiaoyi.sybil@gmail.com>
2019-02-27 17:45:28 -0500

    model

--- baseline_method.py
+++ baseline_method.py
@@ -2,12 +2,16 @@ import sqlite3
 import matplotlib
 matplotlib.use("TkAgg")
 import matplotlib.pyplot as plt
+import pandas as pd
 
 from variable_collections import zillow_db_path, gentrification_db_path, baseline_cache_path, zillow_path
 from cache_management import cache_write, cache_load
 from transfer_csv_to_sql import read_csv_header, create_zillow_table, read_csv_header_list
 
-
+from sklearn.model_selection import train_test_split
+from sklearn.naive_bayes import GaussianNB
+from sklearn.metrics import accuracy_score
+from sklearn.svm import LinearSVC
 
 def query_gentrification(gentrification_db_path):
     conn = sqlite3.connect(gentrification_db_path)
@@ -132,7 +136,8 @@ def create_sql_table_for_useful(table_name, zillow_db_path, bar = 0.1):
     state = "CREATE TABLE IF NOT EXISTS " + table_name
     state += "  (id  CHAR(50) PRIMARY KEY, " \
        " year  INT  , " \
-       " RegionName CHAR(10)" 
+       " RegionName CHAR(10), " \
+       " eligible_gentrification INT"
     for col in list(zillow_metrics.keys()):
         state += "," + str(col) + "   INT "
     state += ")"
@@ -157,6 +162,7 @@ def data_prep_before_insert(baseline_cache_path, bar = 0.1):
     zillow_metrics['id'] = 0
     zillow_metrics['year'] = 2
     zillow_metrics['RegionName'] = 4
+    zillow_metrics['eligible_gentrification'] = 79
     #print(zillow_metrics.keys())
 
     vals_dict = {}
@@ -171,7 +177,9 @@ def data_prep_before_insert(baseline_cache_path, bar = 0.1):
     return vals_dict
 
 
-
+'''
+insert data, without any NA
+'''
 def insert_data_into_table(baseline_cache_path, zillow_db_path, table_name, bar = 0.1):
     vals_dict = data_prep_before_insert(baseline_cache_path, bar)
     
@@ -181,25 +189,52 @@ def insert_data_into_table(baseline_cache_path, zillow_db_path, table_name, bar 
     state = "INSERT INTO " + table_name + " ("
     after = ""
     vals = []
+    order_dict = {}
+    index = 0
     for col in list(vals_dict.keys()):
         state += col
         state += ","
         after += "?,"
         vals.append(vals_dict[col])
+        order_dict[index] = col
+        index += 1
     
     state = state[:-1]
     state += ") VALUES ("
     state += after[:-1]
     state += ");"
 
-    print(state)
-
-    c.execute(state, vals)
-    conn.commit()
-    print("inserted one record successfully")
+    for i in range(len(vals[0])):
+        with_na = False
+        vs = []
+        for index in range(len(vals)):
+            v = vals[index][i]
+            vs.append(v)
+            if order_dict[index] != 'eligible_gentrification' and v == 0:
+                with_na = True
+                break
+        if not with_na:
+            c.execute(state, vs)
+            conn.commit()
+            print("inserted one record successfully")
+        else:
+            print("with na")
 
     conn.close()
         
+
+
+
+def ml_train(db_path, res_col, related_cols, method = 'svm'):
+    conn = sqlite3.connect(db_path)
+    df = pd.read_sql(con = conn, sql="select * from Zillow_gentrification")
+    y = df.pop(res_col)
+    x = df[related_cols]
+    X_train, X_test, y_train, y_test = train_test_split(x, y, random_state=17)
+    gnb = GaussianNB()
+    gnb.fit(X_train, y_train)
+    y_pred = gnb.predict(X_test)
+    print("\nAccuracy score : %f" %(accuracy_score(y_test, y_pred)))
 
 
 if __name__ == '__main__':
@@ -208,13 +243,10 @@ if __name__ == '__main__':
     #print(get_useful_zillow_record())
     #print(choose_zillow_metrics())
     #create_sql_table_for_useful('Zillow_gentrification', zillow_db_path)
+    #data_prep_before_insert(baseline_cache_path)
     #insert_data_into_table(baseline_cache_path, zillow_db_path, 'Zillow_gentrification')
-    h1 = choose_zillow_metrics()
-    for i in range(1000):
-        if h1 != choose_zillow_metrics():
-            print("right")
- 
-
+    cols_needed = list(choose_zillow_metrics().keys())
+    ml_train(zillow_db_path, 'eligible_gentrification', cols_needed)
 
 
 
